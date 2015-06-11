73ceaa4e6dfa4a1c34e1b62f54a78b15221cee0e
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 13:10:58 +0200

    Refactor SourceWorkflow
    This rafactoring brings idempotency to `SourceWorkflow.migrate`. Before
    running the merge twice resulted in duplicate workflow rules.

--- app/models/source_workflow.rb
+++ app/models/source_workflow.rb
@@ -1,53 +1,47 @@
 class SourceWorkflow < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "workflows"
-  
-#  belongs_to :role, :class_name => 'SourceRole', :foreign_key => 'role_id'
-#  belongs_to :tracker, :class_name => 'SourceTracker', :foreign_key => 'tracker_id'
-#  belongs_to :old_status, :class_name => 'SourceIssueStatus', :foreign_key => 'old_status_id'
-#  belongs_to :new_status, :class_name => 'SourceIssueStatus', :foreign_key => 'new_status_id'
-  
+  self.table_name = 'workflows'
+
+  belongs_to :role, :class_name => 'SourceRole', :foreign_key => 'role_id'
+  belongs_to :tracker, :class_name => 'SourceTracker', :foreign_key => 'tracker_id'
+  belongs_to :old_status, :class_name => 'SourceIssueStatus', :foreign_key => 'old_status_id'
+  belongs_to :new_status, :class_name => 'SourceIssueStatus', :foreign_key => 'new_status_id'
+
+  def self.find_target(source_workflow)
+    target_old_status = SourceIssueStatus.find_target(source_workflow.old_status)
+    target_new_status = SourceIssueStatus.find_target(source_workflow.new_status)
+    target_role       = SourceRole.find_target(source_workflow.role)
+    target_tracker    = SourceTracker.find_target(source_workflow.tracker)
+
+    WorkflowRule.first(:conditions => {
+                         old_status_id: target_old_status.id,
+                         new_status_id: target_new_status.id,
+                         role_id:       target_role.id,
+                         tracker_id:    target_tracker.id
+                       })
+  end
+
   def self.migrate
-#    puts "Print out the Source IssueStatus"
-#    SourceIssueStatus.find do |sis|
-#      puts "SourceIssueStatus id = #{sis.id} name = #{sis.name}"
-#    end
-#    
-#    puts "Print out the merged IssueStatus"
-#    IssueStatus.find do |i|
-#      puts "IssueStatus id = #{i.id} name = #{i.name}"
-#    end
-#
-#    puts "Print out the source Roles #{SourceRole.count}"
-#    SourceRole.find do |sr|
-#      puts "Role id = #{sr.id} name = #{sr.name}"
-#    end
-#
-#    puts "Print out the merged Roles #{Role.count}"
-#    Role.find do |r|
-#      puts "Role id = #{r.id} name = #{r.name}"
-#    end
-#    
+    migrated = 0
+    skipped  = 0
     all.each do |source_workflow|
-    
-          old_status = SourceIssueStatus.find_by_id(source_workflow.old_status_id)
-    
-          new_status = SourceIssueStatus.find_by_id(source_workflow.new_status_id)
-    
-          role = SourceRole.find_by_id(source_workflow.role_id)
-    
-          tracker = SourceTracker.find_by_id(source_workflow.tracker_id)
-    
-          WorkflowRule.create!(source_workflow.attributes) do |w|
-            w.old_status = IssueStatus.find_by_name(old_status.name)
-            w.new_status = IssueStatus.find_by_name(new_status.name)
-            w.role = Role.find_by_name(role.name)
-            w.tracker = Tracker.find_by_name(tracker.name)
-            w.type = "WorkflowTransition"
-          end
-    
-        end
-    
+      if SourceWorkflow.find_target(source_workflow)
+        skipped += 1
+        next
       end
-    
+
+      WorkflowRule.create!(source_workflow.attributes) do |w|
+        w.old_status = SourceIssueStatus.find_target(source_workflow.old_status)
+        w.new_status = SourceIssueStatus.find_target(source_workflow.new_status)
+        w.role       = SourceRole.find_target(source_workflow.role)
+        w.tracker    = SourceTracker.find_target(source_workflow.tracker)
+        w.type = 'WorkflowTransition'
+      end
+
+      migrated += 1
     end
+
+    puts "  #{skipped} skipped existing workflow rules"
+    puts "  #{migrated} migrated workflow rules"
+  end
+end
