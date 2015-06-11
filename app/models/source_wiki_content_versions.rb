4e5d5b88a6b6b6920fad3c1dd2dee12bae273316
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 14:01:10 +0200

    Refactor SourceWikiContentVersions

--- app/models/source_wiki_content_versions.rb
+++ app/models/source_wiki_content_versions.rb
@@ -3,16 +3,33 @@ class SourceWikiContentVersions < ActiveRecord::Base
   self.table_name = "wiki_content_versions"
 
   belongs_to :author, :class_name => 'SourceUser', :foreign_key => 'author_id'
+  belongs_to :page, :class_name => 'SourceWikiPage', :foreign_key => 'page_id'
+  belongs_to :wiki_content, :class_name => 'SourceWikiContent', :foreign_key => 'wiki_content_id'
+
+  def self.find_target(source)
+    return nil unless source
+    page = SourceWikiPage.find_target(source.page)
+    WikiContentVersions.find_by_version_and_page_id(source.version, page)
+  end
 
   def self.migrate
-    all.each do |source_wiki_content_version|
-      
-      puts "author login: #{source_wiki_content_version.author.login}  id: #{source_wiki_content_version.author_id}  author.id: #{source_wiki_content_version.author.id}"
-      
-      WikiContentVersions.create!(source_wiki_content_version.attributes) do |wcv|
-        wcv.page_id = RedmineMerge::Mapper.get_new_wiki_page_id(source_wiki_content_version.page_id)
-        wcv.author_id = User.find_by_login(source_wiki_content_version.author.login).id
-        wcv.wiki_content_id = RedmineMerge::Mapper.get_new_wiki_content_id(source_wiki_content_version.wiki_content_id)
+    all.each do |swcv|
+      target_wiki_content_version = SourceWikiContentVersions.find_target(swcv)
+
+      if target_wiki_content_version
+        puts "  Skipping existing content version for #{swcv.page.title} (##{swcv.version})"
+        next
+      end
+
+      puts <<LOG
+  Merging wiki content version for page #{swcv.page.title}
+    author: #{swcv.author}
+    version: #{swcv.version}
+LOG
+      WikiContentVersions.create!(swcv.attributes) do |wcv|
+        wcv.page = SourceWikiPage.find_target(swcv.page)
+        wcv.author_id = SourceUser.find_target(swcv.author).id
+        wcv.wiki_content = SourceWikiContent.find_target(swcv.wiki_content)
       end
     end
   end
