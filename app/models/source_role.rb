25b3ad6003098e1d7e7cf429f8b1869d15df8d3b
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 12:59:57 +0200

    Refactor SourceRole

--- app/models/source_role.rb
+++ app/models/source_role.rb
@@ -1,17 +1,23 @@
-# Created by Ken Sperow
-
 class SourceRole < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "roles"
-  
+  self.table_name = 'roles'
+
   has_many :member_roles, :class_name => 'SourceMemberRoles'
   has_many :members, :through => :member_roles
 
+  def self.find_target(source_role)
+    return nil unless source_role
+    Role.find_by_name(source_role.name)
+  end
+
   def self.migrate
     all.each do |source_role|
-      # Only migrate the role if it doesn't already exist
-      next if Role.find_by_name(source_role.name)
+      if SourceRole.find_target(source_role)
+        puts "  Skipping existing role #{source_role.name}"
+        next
+      end
 
+      puts "  Migrating role #{source_role.name}"
       Role.create!(source_role.attributes)
     end
   end
