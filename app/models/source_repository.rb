90a3c55738e3b1fa0c082ad9ab8474cbf6673a3a
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 13:34:21 +0200

    Refactor SourceRepository
    Theoretically allowing all repository types, checking and enabling
    respective repository type settings.

--- app/models/source_repository.rb
+++ app/models/source_repository.rb
@@ -1,14 +1,37 @@
 class SourceRepository < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "repositories"
+  self.table_name = 'repositories'
+
+  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
+
+  def self.find_target(source_repo)
+    target_url     = source_repo.url
+    target_project = SourceProject.find_target(source_repo.project)
+    Repository.find_by_url_and_project_id(target_url, target_project.id)
+  end
 
   def self.migrate
-    all.each do |source_repository|
+    all.each do |source_repo|
+      if source_repo.project.nil?
+        puts "  Skipping #{source_repo.url}, because project ref is `nil`"
+        next
+      end
 
-      # Note the use of Repository::Git, which sets the type properly.  If we had any other types of repositories in our DB a check would need
-      # to be placed here
-      Repository::Git.create!(source_repository.attributes) do |d|
-        d.project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_repository.project_id))
+      target_repo = SourceRepository.find_target(source_repo)
+
+      if target_repo
+        puts "  Skipping existing repository #{source_repo.url}"
+        next
+      end
+
+      repo_klass = source_repo.type.constantize
+      # Make sure the validations for enabled repo types pass.
+      Setting.enabled_scm =
+        Setting.enabled_scm.push(repo_klass.name.demodulize).uniq
+
+      puts "  Migrating repository #{source_repo.url} for project #{source_repo.project.name}"
+      repo_klass.create!(source_repo.attributes) do |d|
+        d.project = SourceProject.find_target(source_repo.project)
       end
     end
   end
