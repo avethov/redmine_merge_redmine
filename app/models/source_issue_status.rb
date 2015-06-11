aa1c80f9f01a8d51fb7c020e8be5d54cb8358971
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 12:57:14 +0200

    Refactor SourceIssueStatus

--- app/models/source_issue_status.rb
+++ app/models/source_issue_status.rb
@@ -2,9 +2,17 @@ class SourceIssueStatus < ActiveRecord::Base
   include SecondDatabase
   set_table_name :issue_statuses
 
+  def self.find_target(source_issue_status)
+    return nil unless source_issue_status
+    IssueStatus.find_by_name(source_issue_status.name)
+  end
+
   def self.migrate
     all.each do |source_issue_status|
-      next if IssueStatus.find_by_name(source_issue_status.name)
+      if SourceIssueStatus.find_target(source_issue_status)
+        puts "  Skipping existing issue status #{source_issue_status.name}"
+        next
+      end
 
       IssueStatus.create!(source_issue_status.attributes)
     end
