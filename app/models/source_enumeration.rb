c7ef0eb1a4787309c5c4a44dc65e30c269461f51
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 13:57:36 +0200

    Refactor SourceEnumeration
    DRY'ed up the code and added some more verbosity.

--- app/models/source_enumeration.rb
+++ app/models/source_enumeration.rb
@@ -2,44 +2,41 @@ class SourceEnumeration < ActiveRecord::Base
   include SecondDatabase
   set_table_name :enumerations
 
-  # KS TODO - need to handle issue project_id and parent_id
-  def self.migrate_issue_priorities
-    all(:conditions => {:type => "IssuePriority"}) .each do |source_issue_priority|
-      next if IssuePriority.find_by_name(source_issue_priority.name)
-      
-      # Added by KS
-      project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_issue_priority.project_id)) if source_issue_priority.project_id
+  ALLOWED_TYPES = %w(IssuePriority TimeEntryActivity DocumentCategory)
 
-      IssuePriority.create!(source_issue_priority.attributes) do |i|
-        i.project = project if project
+  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
+
+  def self.find_target(source_enum)
+    fail "Unknown enum type #{source_enum.type}" unless ALLOWED_TYPES.include?(source_enum.type)
+    project = SourceProject.find_target(source_enum.project)
+    conditions = { :name => source_enum.name }
+    conditions[:project_id] = project.id if project
+    source_enum.type.constantize.first(:conditions => conditions)
+  end
+
+  def self.migrate_enum(type)
+    klass = type.constantize
+    all(:conditions => { :type => type }).each do |source_enum|
+      if SourceEnumeration.find_target(source_enum)
+        puts "  Skipping existing #{type} with name #{source_enum.name}"
+        next
+      end
+
+      klass.create!(source_enum.attributes) do |i|
+        i.project = SourceProject.find_target(source_enum.project)
       end
     end
+  end
+
+  def self.migrate_issue_priorities
+    migrate_enum('IssuePriority')
   end
 
   def self.migrate_time_entry_activities
-    all(:conditions => {:type => "TimeEntryActivity"}) .each do |source_activity|
-      next if TimeEntryActivity.find_by_name(source_activity.name)
-
-      # Added by KS
-      project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_activity.project_id)) if source_activity.project_id
-      
-      TimeEntryActivity.create!(source_activity.attributes) do |a|
-        a.project = project
-      end
-    end
+    migrate_enum('TimeEntryActivity')
   end
 
   def self.migrate_document_categories
-    all(:conditions => {:type => "DocumentCategory"}) .each do |source_document_category|
-      next if DocumentCategory.find_by_name(source_document_category.name)
-
-      # Added by KS
-      project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_document_category.project_id)) if source_document_category.project_id
-
-      DocumentCategory.create!(source_document_category.attributes)  do |d|
-        d.project = project if project
-      end
-    end
+    migrate_enum('DocumentCategory')
   end
-
 end
