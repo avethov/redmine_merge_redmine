b73997c6dfe42c5ea40c21f918c12ca6fe9c6c4d
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 13:57:08 +0200

    Refactor SourceIssueCategory
    Make the migration idempotent.

--- app/models/source_issue_category.rb
+++ app/models/source_issue_category.rb
@@ -2,14 +2,25 @@ class SourceIssueCategory < ActiveRecord::Base
   include SecondDatabase
   set_table_name :issue_categories
 
-  # KS - logic here seems flawed - they only create a new record if the category_name and "source" project_id are not 
-  # in the database.  However, it seems like the mapped project id should be found and then the check done.  Also, I don't see the find_by_name_and_project_id method anywhere -- is it obsolete?
+  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
+
+  def self.find_target(source_issue_category)
+    project = SourceProject.find_target(source_issue_category.project)
+    IssueCategory.find_by_name_and_project_id(source_issue_category.name, project)
+  end
+
   def self.migrate
     all.each do |source_issue_category|
-      next if IssueCategory.find_by_name_and_project_id(source_issue_category.name, source_issue_category.project_id)
+      target_issue_category = SourceIssueCategory.find_target(source_issue_category)
 
+      if target_issue_category
+        puts "  Skipping existing issue category #{source_issue_category.name} for project #{source_issue_category.project.name}"
+        next
+      end
+
+      puts "  Migrating issue category #{source_issue_category.name} for project #{source_issue_category.project.name}"
       IssueCategory.create!(source_issue_category.attributes) do |ic|
-        ic.project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_issue_category.project_id))
+        ic.project = SourceProject.find_target(source_issue_category.project)
       end
     end
   end
