6112d5e2dafa6475514feb3beffc0168e3ef8fa4
Arthur Andersen <leoc.git@gmail.com>
2015-06-11 14:03:41 +0200

    Add SourcePrincipal.find_target

--- app/models/source_principal.rb
+++ app/models/source_principal.rb
@@ -1,4 +1,12 @@
 class SourcePrincipal < ActiveRecord::Base
-    include SecondDatabase
-    self.table_name = "users"
+  include SecondDatabase
+  self.table_name = "users"
+
+  def self.find_target(source_principal, options = { fail: true })
+    target = User.find_by_mail(source_principal.mail) || User.find_by_login(source_principal.login)
+    if target.nil? && options[:fail]
+      fail "No target found with mail `#{source_principal.mail}` or login `#{source_principal.login}`"
+    end
+    target
+  end
 end
