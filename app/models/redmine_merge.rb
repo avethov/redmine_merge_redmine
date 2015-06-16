bdd3a60d03325515f13959b23a755c658fbe676c
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:04:08 +0200

    Refactor RedmineMerge::Mapper

--- app/models/redmine_merge.rb
+++ app/models/redmine_merge.rb
@@ -120,86 +120,20 @@ class RedmineMerge
   end
 
   class Mapper
-    Projects = {}
-    Issues = {}
-    Journals = {}
-    Wikis = {}
-    WikiPages = {}
-    WikiContent = {}
-    Documents = {}
-    Versions = {}
-    News = {}
-
-    def self.add_project(source_id, new_id)
-      Projects[source_id] = new_id
+    def self.mappings
+      @mappings ||= {}
     end
 
-    def self.get_new_project_id(source_id)
-      Projects[source_id]
+    def self.mapping(table_name)
+      mappings[table_name] ||= {}
     end
 
-    def self.add_issue(source_id, new_id)
-      Issues[source_id] = new_id
+    def self.map(source, target)
+      mapping(source.class.table_name)[source.id] = target.id
     end
 
-    def self.get_new_issue_id(source_id)
-      Issues[source_id]
-    end
-
-    def self.add_journal(source_id, new_id)
-      Journals[source_id] = new_id
-    end
-
-    def self.get_new_journal_id(source_id)
-      Journals[source_id]
-    end
-
-    def self.add_wiki(source_id, new_id)
-      Wikis[source_id] = new_id
-    end
-
-    def self.get_new_wiki_id(source_id)
-      Wikis[source_id]
-    end
-
-    def self.add_wiki_content(source_id, new_id)
-      WikiContent[source_id] = new_id
-    end
-
-    def self.get_new_wiki_content_id(source_id)
-      WikiContent[source_id]
-    end
-
-    def self.add_wiki_page(source_id, new_id)
-      WikiPages[source_id] = new_id
-    end
-
-    def self.get_new_wiki_page_id(source_id)
-      WikiPages[source_id]
-    end
-
-    def self.add_document(source_id, new_id)
-      Documents[source_id] = new_id
-    end
-
-    def self.get_new_document_id(source_id)
-      Documents[source_id]
-    end
-
-    def self.add_version(source_id, new_id)
-      Versions[source_id] = new_id
-    end
-
-    def self.get_new_version_id(source_id)
-      Versions[source_id]
-    end
-
-    def self.add_news(source_id, new_id)
-      News[source_id] = new_id
-    end
-
-    def self.get_new_news_id(source_id)
-      News[source_id]
+    def self.target_id(source)
+      mapping(source.class.table_name)[source.id]
     end
 
     # Add logic to replace any issue number found within the description (e.g.,#1234) with the new issue number
@@ -211,65 +145,11 @@ class RedmineMerge
       refs = str.scan(/#([\d]+)/).flatten
       refs.each do |ref|
         source_issue_id = ref.gsub(/#/, '').to_i
-        target_issue_id = RedmineMerge::Mapper.get_new_issue_id(source_issue_id)
+        target_issue_id = mapping('issues')[source_issue_id]
         puts "=> Updating ref #{ref} with ##{target_issue_id}"
         str.gsub!("#{ref}", "##{target_issue_id}") if target_issue_id
       end
       str
-    end
-
-    def self.find_id_by_property(target_klass, source_id)
-      # Similar to issues_helper.rb#show_detail
-      source_id = source_id.to_i
-
-      case target_klass.to_s
-      when 'Project'
-        return Mapper.get_new_journal_id(source_id)
-      when 'IssueStatus'
-        target = find_target_record_from_source(SourceIssueStatus, IssueStatus, :name, source_id)
-        return target.id if target
-        return nil
-      when 'Tracker'
-        target = find_target_record_from_source(SourceTracker, Tracker, :name, source_id)
-        return target.id if target
-        return nil
-      when 'User'
-        target = find_target_record_from_source(SourceUser, User, :login, source_id)
-        return target.id if target
-        return nil
-      when 'News'
-        return Mapper.get_new_news_id(source_id)
-      when 'Enumeration'
-        target = find_target_record_from_source(SourceEnumeration, Enumeration, :name, source_id)
-        return target.id if target
-        return nil
-      when 'IssueCategory'
-        source = SourceIssueCategory.find_by_id(source_id)
-        return nil unless source
-        target = IssueCategory.find_by_name_and_project_id(source.name, RedmineMerge::Mapper.get_new_project_id(source.project_id))
-        return target.id if target
-        return nil
-      when 'Version'
-        source = SourceVersion.find_by_id(source_id)
-        return nil unless source
-        target = Version.find_by_name_and_project_id(source.name, RedmineMerge::Mapper.get_new_project_id(source.project_id))
-        return target.id if target
-        return nil
-      end
-    end
-
-    private
-
-    # Utility method to dynamically find the target records
-    def self.find_target_record_from_source(source_klass, target_klass, field, source_id)
-
-      source = source_klass.find_by_id(source_id)
-      field = field.to_sym
-      if source
-        return target_klass.find(:first, :conditions => {field => source.read_attribute(field) })
-      else
-        return nil
-      end
     end
   end
 end
