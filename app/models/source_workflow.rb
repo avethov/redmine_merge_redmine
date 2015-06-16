676f4d1e01ca8eabea6b7b0c750d5a34e759a7b1
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:12:39 +0200

    Fix SourceWorkflow for Redmine 3

--- app/models/source_workflow.rb
+++ app/models/source_workflow.rb
@@ -2,23 +2,18 @@ class SourceWorkflow < ActiveRecord::Base
   include SecondDatabase
   self.table_name = 'workflows'
 
-  belongs_to :role, :class_name => 'SourceRole', :foreign_key => 'role_id'
-  belongs_to :tracker, :class_name => 'SourceTracker', :foreign_key => 'tracker_id'
-  belongs_to :old_status, :class_name => 'SourceIssueStatus', :foreign_key => 'old_status_id'
-  belongs_to :new_status, :class_name => 'SourceIssueStatus', :foreign_key => 'new_status_id'
+  belongs_to :role,       class_name: 'SourceRole'
+  belongs_to :tracker,    class_name: 'SourceTracker'
+  belongs_to :old_status, class_name: 'SourceIssueStatus'
+  belongs_to :new_status, class_name: 'SourceIssueStatus'
 
   def self.find_target(source_workflow)
-    target_old_status = SourceIssueStatus.find_target(source_workflow.old_status)
-    target_new_status = SourceIssueStatus.find_target(source_workflow.new_status)
-    target_role       = SourceRole.find_target(source_workflow.role)
-    target_tracker    = SourceTracker.find_target(source_workflow.tracker)
-
-    WorkflowRule.first(:conditions => {
-                         old_status_id: target_old_status.id,
-                         new_status_id: target_new_status.id,
-                         role_id:       target_role.id,
-                         tracker_id:    target_tracker.id
-                       })
+    WorkflowRule.where(
+      old_status_id: SourceIssueStatus.find_target(source_workflow.old_status),
+      new_status_id: SourceIssueStatus.find_target(source_workflow.new_status),
+      role_id:       SourceRole.find_target(source_workflow.role),
+      tracker_id:    SourceTracker.find_target(source_workflow.tracker)
+    ).first
   end
 
   def self.migrate
