4553749c4b0f7f70fed0d0bb7413833e88cfc0e8
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:26:07 +0200

    Fix SourceWikiContent for Redmine 3

--- app/models/source_wiki_content.rb
+++ app/models/source_wiki_content.rb
@@ -2,35 +2,35 @@ class SourceWikiContent < ActiveRecord::Base
   include SecondDatabase
   self.table_name = 'wiki_contents'
 
-  belongs_to :author, :class_name => 'SourceUser', :foreign_key => 'author_id'
-  belongs_to :page, :class_name => 'SourceWikiPage', :foreign_key => 'page_id'
+  belongs_to :author, class_name: 'SourceUser', foreign_key: 'author_id'
+  belongs_to :page, class_name: 'SourceWikiPage', foreign_key: 'page_id'
 
-  def self.find_target(source_wiki_content)
-    return nil unless source_wiki_content
-    page = SourceWikiPage.find_target(source_wiki_content.page)
-    WikiContent.find_by_id(RedmineMerge::Mapper.get_new_wiki_content_id(source_wiki_content.id)) ||
-      WikiContent.find_by_page_id(page.id)
+  def self.find_target(source)
+    return nil unless source
+    WikiContent.find_by_id(RedmineMerge::Mapper.target_id(source)) ||
+      WikiContent.where(
+        page_id: SourceWikiPage.find_target(source.page)
+      ).first
   end
 
   def self.migrate
-    all.each do |source_wiki_content|
-      target_wiki_content = SourceWikiContent.find_target(source_wiki_content)
-      if target_wiki_content
-        puts "  Skipping existing wiki content for page #{source_wiki_content.page.title} by #{source_wiki_content.author}"
+    all.each do |source|
+      target = SourceWikiContent.find_target(source)
+      if target
+        puts "  Skipping existing wiki content for page #{source.page.title} by #{source.author}"
       else
         puts <<LOG
-  Migrating wiki content for page #{source_wiki_content.page.title}
-    id: #{source_wiki_content.author_id}
-    author: #{source_wiki_content.author} (#{source_wiki_content.author.id})
-    version: #{source_wiki_content.version}
+  Migrating wiki content for page #{source.page.title}
+    author: #{source.author} (#{source.author.id})
+    version: #{source.version}
 LOG
-        target_wiki_content = WikiContent.create!(source_wiki_content.attributes) do |wc|
-          wc.page = SourceWikiPage.find_target(source_wiki_content.page)
-          wc.author = SourceUser.find_target(source_wiki_content.author)
+        target = WikiContent.create!(source.attributes) do |wc|
+          wc.page = SourceWikiPage.find_target(source.page)
+          wc.author = SourceUser.find_target(source.author)
         end
       end
 
-      RedmineMerge::Mapper.add_wiki_content(source_wiki_content.id, target_wiki_content.id)
+      RedmineMerge::Mapper.map(source, target)
     end
   end
 end
