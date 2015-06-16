d380d30ad08fbdb8efd38e6593af46e0089977bd
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:26:15 +0200

    Fix SourceWikiContentVersions for Redmine 3

--- app/models/source_wiki_content_versions.rb
+++ app/models/source_wiki_content_versions.rb
@@ -1,35 +1,40 @@
 class SourceWikiContentVersions < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "wiki_content_versions"
+  self.table_name = 'wiki_content_versions'
 
-  belongs_to :author, :class_name => 'SourceUser', :foreign_key => 'author_id'
-  belongs_to :page, :class_name => 'SourceWikiPage', :foreign_key => 'page_id'
-  belongs_to :wiki_content, :class_name => 'SourceWikiContent', :foreign_key => 'wiki_content_id'
+  belongs_to :author, class_name: 'SourceUser', foreign_key: 'author_id'
+  belongs_to :page, class_name: 'SourceWikiPage', foreign_key: 'page_id'
+  belongs_to :wiki_content, class_name: 'SourceWikiContent', foreign_key: 'wiki_content_id'
 
   def self.find_target(source)
     return nil unless source
-    page = SourceWikiPage.find_target(source.page)
-    WikiContentVersions.find_by_version_and_page_id(source.version, page)
+    WikiContentVersions.where(
+      version: source.version,
+      page_id: SourceWikiPage.find_target(source.page)
+    ).first
+  end
+
+  def self.create_target(source)
+    puts "  Migrating wiki content version for page #{source.page.title} version #{source.version} by #{source.author}"
+    WikiContentVersions.create!(source.attributes) do |target|
+      target.page = SourceWikiPage.find_target(source.page)
+      target.author = SourceUser.find_target(source.author)
+      target.wiki_content = SourceWikiContent.find_target(source.wiki_content)
+
+      # Needs to be set explicitly, otherwise postgres laments
+      # about a null value for the not-null field `version`.
+      target.version = source.version
+
+      target.updated_on = source.updated_on
+    end
   end
 
   def self.migrate
-    all.each do |swcv|
-      target_wiki_content_version = SourceWikiContentVersions.find_target(swcv)
-
-      if target_wiki_content_version
-        puts "  Skipping existing content version for #{swcv.page.title} (##{swcv.version})"
-        next
-      end
-
-      puts <<LOG
-  Merging wiki content version for page #{swcv.page.title}
-    author: #{swcv.author}
-    version: #{swcv.version}
-LOG
-      WikiContentVersions.create!(swcv.attributes) do |wcv|
-        wcv.page = SourceWikiPage.find_target(swcv.page)
-        wcv.author_id = SourceUser.find_target(swcv.author).id
-        wcv.wiki_content = SourceWikiContent.find_target(swcv.wiki_content)
+    all.each do |source|
+      if SourceWikiContentVersions.find_target(source)
+        puts "  Skipping existing content version for #{source.page.title} (##{source.version})"
+      else
+        create_target(source)
       end
     end
   end
