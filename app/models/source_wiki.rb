181b42357bd7703b5983566b0f6ddcfc369421d7
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:25:36 +0200

    Fix SourceWiki for Redmine 3

--- app/models/source_wiki.rb
+++ app/models/source_wiki.rb
@@ -1,30 +1,31 @@
 class SourceWiki < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "wikis"
+  self.table_name = 'wikis'
 
-  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
+  belongs_to :project, class_name: 'SourceProject', foreign_key: 'project_id'
 
-  def self.find_target(source_wiki)
-    return nil unless source_wiki
-    project = SourceProject.find_target(source_wiki.project)
-    Wiki.find_by_id(RedmineMerge::Mapper.get_new_wiki_id(source_wiki.id)) ||
-      Wiki.find_by_project_id(project.id)
+  def self.find_target(source)
+    return nil unless source
+    Wiki.find_by_id(RedmineMerge::Mapper.target_id(source)) ||
+      Wiki.where(
+        project_id: SourceProject.find_target(source.project)
+    ).first
   end
 
   def self.migrate
-    all.each do |source_wiki|
-      target_wiki = SourceWiki.find_target(source_wiki)
+    all.each do |source|
+      target = find_target(source)
 
-      if target_wiki
-        puts "  Skipping existing wiki for project #{target_wiki.project.name}"
+      if target
+        puts "  Skipping existing wiki for project #{target.project.name}"
       else
-        puts "  Migrating wiki for project #{source_wiki.project.name}"
-        target_wiki = Wiki.create!(source_wiki.attributes) do |w|
-          w.project = SourceProject.find_target(source_wiki.project)
+        puts "  Migrating wiki for project #{source.project.name}"
+        target = Wiki.create!(source.attributes) do |w|
+          w.project = SourceProject.find_target(source.project)
         end
       end
 
-      RedmineMerge::Mapper.add_wiki(source_wiki.id, target_wiki.id)
+      RedmineMerge::Mapper.map(source, target)
     end
   end
 end
