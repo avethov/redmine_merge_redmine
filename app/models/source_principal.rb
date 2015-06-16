44bb1886f77321f55038d179e4449fb28704360a
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:24:19 +0200

    Fix SourcePrincipal::find_target

--- app/models/source_principal.rb
+++ app/models/source_principal.rb
@@ -1,12 +1,12 @@
 class SourcePrincipal < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "users"
+  self.table_name = 'users'
 
-  def self.find_target(source_principal, options = { fail: true })
-    target = User.find_by_mail(source_principal.mail) || User.find_by_login(source_principal.login)
-    if target.nil? && options[:fail]
-      fail "No target found with mail `#{source_principal.mail}` or login `#{source_principal.login}`"
+  def self.find_target(source)
+    return nil unless source
+    case source.type
+    when 'Group' then SourceGroup.find_target(source)
+    when 'User' then SourceUser.find_target(source, fail: false)
     end
-    target
   end
 end
