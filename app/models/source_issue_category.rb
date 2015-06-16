6c020c79d95b148f74b37fc899039a7350072926
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:24:41 +0200

    Fix SourceIssueCategory for Redmine 3

--- app/models/source_issue_category.rb
+++ app/models/source_issue_category.rb
@@ -2,25 +2,28 @@ class SourceIssueCategory < ActiveRecord::Base
   include SecondDatabase
   self.table_name = 'issue_categories'
 
-  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
+  belongs_to :project, class_name: 'SourceProject', foreign_key: 'project_id'
 
-  def self.find_target(source_issue_category)
-    project = SourceProject.find_target(source_issue_category.project)
-    IssueCategory.find_by_name_and_project_id(source_issue_category.name, project)
+  def self.find_target(source)
+    return nil unless source
+    IssueCategory.where(
+      name: source.name,
+      project_id: SourceProject.find_target(source.project)
+    ).first
   end
 
   def self.migrate
-    all.each do |source_issue_category|
-      target_issue_category = SourceIssueCategory.find_target(source_issue_category)
+    all.each do |source|
+      target_issue_category = SourceIssueCategory.find_target(source)
 
       if target_issue_category
-        puts "  Skipping existing issue category #{source_issue_category.name} for project #{source_issue_category.project.name}"
+        puts "  Skipping existing issue category #{source.name} for project #{source.project.name}"
         next
       end
 
-      puts "  Migrating issue category #{source_issue_category.name} for project #{source_issue_category.project.name}"
-      IssueCategory.create!(source_issue_category.attributes) do |ic|
-        ic.project = SourceProject.find_target(source_issue_category.project)
+      puts "  Migrating issue category #{source.name} for project #{source.project.name}"
+      IssueCategory.create!(source.attributes) do |ic|
+        ic.project = SourceProject.find_target(source.project)
       end
     end
   end
