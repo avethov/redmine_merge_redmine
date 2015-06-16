5b7ac55364ae41526ed2c7996dc05bed0e2978b6
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:26:40 +0200

    Clean up SourceWikiRedirects

--- app/models/source_wiki_redirect.rb
+++ app/models/source_wiki_redirect.rb
@@ -2,13 +2,30 @@
 
 class SourceWikiRedirect < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "wiki_redirects"
+  self.table_name = 'wiki_redirects'
+
+  belongs_to :wiki, class_name: 'SourceWiki', foreign_key: 'wiki_id'
+
+  def self.find_target(source)
+    return nil unless source
+    WikiRedirect.where(
+      title: source.title,
+      wiki_id: SourceWiki.find_target(source.wiki),
+      created_on: source.created_on
+    ).first
+  end
 
   def self.migrate
-    all.each do |source_wiki_redirect|
-      
-      wiki_page = WikiRedirect.create!(source_wiki_redirect.attributes) do |wr|
-        wr.wiki = Wiki.find(RedmineMerge::Mapper.get_new_wiki_id(source_wiki_redirect.wiki_id))
+    all.each do |source|
+      target = find_target(source)
+      if target
+        puts "  Skipping existing wiki redirect #{target.title}"
+        next
+      end
+
+      puts "  Migrating wiki redirect #{target.title}"
+      WikiRedirect.create!(source.attributes) do |wr|
+        wr.wiki = SourceWiki.find_target(source.wiki)
       end
     end
   end
