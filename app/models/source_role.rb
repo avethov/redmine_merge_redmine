431887c9c6c716c21a2cef08d1f466554d63b13d
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:10:05 +0200

    Fix SourceRole permission migration
    The `Permission` model seems to handle the permission array as yaml,
    that's the reason why we cannot simple mass assign source attributes.

--- app/models/source_role.rb
+++ app/models/source_role.rb
@@ -5,6 +5,10 @@ class SourceRole < ActiveRecord::Base
   has_many :member_roles, :class_name => 'SourceMemberRoles'
   has_many :members, :through => :member_roles
 
+  def permissions
+    read_attribute(:permissions).to_s.scan(/:([a-z0-9_]+)/).flatten.map(&:to_sym)
+  end
+
   def self.find_target(source_role)
     return nil unless source_role
     Role.find_by_name(source_role.name)
@@ -18,7 +22,9 @@ class SourceRole < ActiveRecord::Base
       end
 
       puts "  Migrating role #{source_role.name}"
-      Role.create!(source_role.attributes)
+      Role.create!(source_role.attributes.dup.except('permissions')) do |r|
+        r.permissions = source_role.permissions
+      end
     end
   end
 end
