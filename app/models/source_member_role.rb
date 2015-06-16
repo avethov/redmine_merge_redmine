116b49177f1cee9fccc0cc22af7dec09ecc108b0
Arthur Andersen <leoc.git@gmail.com>
2015-06-16 19:16:59 +0200

    Add SourceMemberRole::find_target

--- app/models/source_member_role.rb
+++ app/models/source_member_role.rb
@@ -3,7 +3,14 @@ class SourceMemberRole < ActiveRecord::Base
   self.table_name = "member_roles"
   belongs_to :member, :class_name => 'SourceMember', :foreign_key => 'member_id'
   belongs_to :role, :class_name => 'SourceRole', :foreign_key => 'role_id'
-  
+
+  def self.find_target(source_member_role)
+    return nil unless source_member_role
+    MemberRole.where(
+      member_id: SourceMember.find_target(source_member_role.member),
+      role_id: SourceRole.find_target(source_member_role.role)
+    ).first
+  end
 
   def inherited?
     !inherited_from.nil?
