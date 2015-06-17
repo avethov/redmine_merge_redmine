50e8371ee135198dea3cb27f6f4cf9a2c67dca90
Arthur Andersen <leoc.git@gmail.com>
2015-06-17 08:51:01 +0200

    Fix SourceCustomValue for Redmine 3

--- app/models/source_custom_value.rb
+++ app/models/source_custom_value.rb
@@ -1,29 +1,42 @@
 class SourceCustomValue < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "custom_values"
+  self.table_name = 'custom_values'
 
-  belongs_to :custom_field, :class_name => 'SourceCustomField', :foreign_key => 'custom_field_id'
-  belongs_to :customized, :polymorphic => true
+  CUSTOMIZABLE_TYPES = %w(Issue Document WikiPage Project Version News)
+
+  belongs_to :custom_field, class_name: 'SourceCustomField', foreign_key: 'custom_field_id'
+
+  def self.find_target(source)
+    return nil unless source
+    CustomValue.where(
+      customized_type: source.customized_type,
+      customized_id: source.customized.class.find_target(source.customized),
+      custom_field_id: SourceCustomField.find_target(source.custom_field),
+    ).first
+  end
+
+  # Needs to be a custom relation accessor because the `polymorphic`
+  # option cannot use source models for `SourceCustomValue`.
+  def customized
+    @customized_memo ||=
+      begin
+        source_klass = "Source#{customized_type}".constantize
+        source_klass.find_by_id(customized_id)
+      end
+  end
 
   def self.migrate
-    all.each do |source_custom_value|
-      
-      CustomValue.create!(source_custom_value.attributes) do |cv|
-        cv.custom_field = CustomField.find_by_name(source_custom_value.custom_field.name)
-        cv.customized = case source_custom_value.customized_type
-                      when "Issue"
-                        Issue.find RedmineMerge::Mapper.get_new_issue_id(source_custom_value.customized_id)
-                      when "Document"
-                        Document.find RedmineMerge::Mapper.get_new_document_id(source_custom_value.customized_id)
-                      when "WikiPage"
-                        WikiPage.find RedmineMerge::Mapper.get_new_wiki_page_id(source_custom_value.customized_id)
-                      when "Project"
-                        Project.find RedmineMerge::Mapper.get_new_project_id(source_custom_value.customized_id)
-                      when "Version"
-                        Version.find RedmineMerge::Mapper.get_new_version_id(source_custom_value.customized_id)
-                      when "News"
-                        News.find RedmineMerge::Mapper.get_new_news_id(source_custom_value.customized_id)
-                      end
+    all.each do |source|
+      target = find_target(source)
+      if target
+        puts "  Skipping existing custom value #{target.custom_field.name}"
+        next
+      end
+
+      puts "  Migrating custom value for field #{source.custom_field.name}"
+      CustomValue.create!(source.attributes) do |cv|
+        cv.custom_field = SourceCustomField.find_target(source.custom_field)
+        cv.customized   = source.customized.class.find_target(source.customized)
       end
     end
   end
