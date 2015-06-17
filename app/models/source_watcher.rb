cc78a8c4a40c571b7d87d0b0f13ff7929a5cb549
Arthur Andersen <leoc.git@gmail.com>
2015-06-17 08:50:49 +0200

    Fix SourceWatcher for Redmine 3

--- app/models/source_watcher.rb
+++ app/models/source_watcher.rb
@@ -1,54 +1,59 @@
 # Added by Ken Sperow
 # Depends on user and watchable_id, which can point to an issue, wiki_page, or news
 
-
-
 class SourceWatcher < ActiveRecord::Base
   include SecondDatabase
-  self.table_name = "watchers"
+  self.table_name = 'watchers'
 
-  belongs_to :watchable, :polymorphic => true
-  belongs_to :user, :class_name => 'SourceUser', :foreign_key => 'user_id'
+  WATCHABLE_TYPES = %w(Issue Document WikiPage Project Version News)
 
+  belongs_to :user, class_name: 'SourceUser', foreign_key: 'user_id'
+
+  def self.find_target(source)
+    return nil unless source
+    Watcher.where(
+      user_id: SourceUser.find_target(source.user),
+      watchable_id: find_watchable_target(source.watchable_type, source.watchable)
+    ).first
+  end
+
+  # Needs to be a custom relation accessor because the `polymorphic`
+  # option cannot use source models for `SourceWatcher`.
+  def watchable
+    source_klass = "Source#{watchable_type}".constantize
+    source_klass.find_by_id(watchable_id)
+  end
+
+  def self.find_watchable_target(type, source)
+    fail "Unknown watchable type: #{type}" unless WATCHABLE_TYPES.include?(type)
+    source_klass = "Source#{type}".constantize
+    source_klass.find_target(source)
+  end
 
   def self.migrate
-    all.each do |source_watcher|
-
-      puts "migrating watcher, source_watcher.id: #{source_watcher.id}"
-      puts "  source_watcher.watchable_type: #{source_watcher.watchable_type}"
-      puts "  source_watcher.watchable_id: #{source_watcher.watchable_id}"
-      puts "  source_watcher.user.login: #{source_watcher.user.login}"
-      
-      # Moved these above because validation required them to be set properly when creating the migrated watcher
-      # as opposed to doing this in the do block
-      temp_user = User.find_by_login(source_watcher.user.login)
-      source_watcher.user_id = temp_user.id
-      
-      # In VLab thus far we are not dealing with messages (so skip them if there happen to be any)
-      next if source_watcher.watchable_type == "Message"
-      
-      source_watcher.watchable = case source_watcher.watchable_type
-                    when "Issue"
-                      Issue.find RedmineMerge::Mapper.get_new_issue_id(source_watcher.watchable_id)
-                    when "Document"
-                      Document.find RedmineMerge::Mapper.get_new_document_id(source_watcher.watchable_id)
-                    when "WikiPage"
-                      WikiPage.find RedmineMerge::Mapper.get_new_wiki_page_id(source_watcher.watchable_id)
-                    when "Project"
-                      Project.find RedmineMerge::Mapper.get_new_project_id(source_watcher.watchable_id)
-                    when "Version"
-                      Version.find RedmineMerge::Mapper.get_new_version_id(source_watcher.watchable_id)
-                    when "News"
-                      News.find RedmineMerge::Mapper.get_new_news_id(source_watcher.watchable_id)
-                    end
-
-      puts "  merged_watcher.watchable_type: #{source_watcher.watchable_type}"
-      puts "  merged_watcher.watchable_id: #{source_watcher.watchable_id}"
-      puts "  merged_watcher.user.login: #{temp_user.login}"
+    all.each do |source|
+      target = find_target(source)
+      if target
+        puts '  Skipping existing watcher'
+        next
+      end
 
       # Don't migrate watcher entries for News events where the News author is the watcher
-      if !((source_watcher.watchable_type == "News") && (source_watcher.watchable.author.login == temp_user.login))
-        Watcher.create!(source_watcher.attributes)
+      if source.watchable_type == 'News' && source.watchable.author == source.user
+        puts '  Skipping watcher for news author'
+        next
+      end
+
+      user = SourceUser.find_target(source.user)
+      # The `Watcher` model checks for the `active?` state while validating.
+      unless user.active?
+        puts '  Skipping watcher for inactive user'
+        next
+      end
+
+      Watcher.create!(source.attributes) do |w|
+        w.watchable = find_watchable_target(source.watchable_type, source.watchable)
+        w.user      = user
       end
     end
   end
