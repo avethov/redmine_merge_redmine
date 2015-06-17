37a29d57aa13f31d77788bffcf7578191b23088f
Arthur Andersen <leoc.git@gmail.com>
2015-06-17 10:24:46 +0200

    Add missing TargetIssue to avoid issue validations

---
+++ app/models/target_issue.rb
@@ -0,0 +1,15 @@
+# We use TargetIssue so we can set `updated_on` and `created_on`
+# manually, which would be overridden by Redmines original `Issue`
+# model.
+class TargetIssue < ActiveRecord::Base
+  self.table_name = 'issues'
+
+  belongs_to :project
+  belongs_to :tracker
+  belongs_to :status, :class_name => 'IssueStatus'
+  belongs_to :author, :class_name => 'User'
+  belongs_to :assigned_to, :class_name => 'Principal'
+  belongs_to :fixed_version, :class_name => 'Version'
+  belongs_to :priority, :class_name => 'IssuePriority'
+  belongs_to :category, :class_name => 'IssueCategory'
+end
