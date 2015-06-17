d9f6e6d9071df2814f2ef6b5c656a5276c901a1d
Arthur Andersen <leoc.git@gmail.com>
2015-06-17 10:43:05 +0200

    Fix SourceAttachment for Redmine 3

--- app/models/source_attachment.rb
+++ app/models/source_attachment.rb
@@ -2,28 +2,38 @@ class SourceAttachment < ActiveRecord::Base
   include SecondDatabase
   self.table_name = 'attachments'
 
-  belongs_to :author, :class_name => 'SourceUser', :foreign_key => 'author_id'
+  belongs_to :author, class_name: 'SourceUser', foreign_key: 'author_id'
+
+  def self.find_target(source)
+    return nil unless source
+    Attachment.where(
+      author_id: SourceUser.find_target(source.author),
+      container_id: source.container.class.find_target(source.container),
+      container_type: source.container_type,
+      filename: source.filename,
+      created_on: source.created_on
+    ).first
+  end
+
+  # Needs to be a custom relation accessor because the `polymorphic`
+  # option cannot use source models for `SourceAttachment`.
+  def container
+    source_klass = "Source#{container_type}".constantize
+    source_klass.find_by_id(container_id)
+  end
 
   def self.migrate
-    all.each do |source_attachment|
+    all.each do |source|
+      target = find_target(source)
+      if target
+        puts "  Skipping existing attachment for #{source.container}"
+        next
+      end
 
-      Attachment.create!(source_attachment.attributes) do |a|
-        a.author = User.find_by_login(source_attachment.author.login)
-        # KS - added "News" to case statement below
-        a.container = case source_attachment.container_type
-                      when "Issue"
-                        Issue.find RedmineMerge::Mapper.get_new_issue_id(source_attachment.container_id)
-                      when "Document"
-                        Document.find RedmineMerge::Mapper.get_new_document_id(source_attachment.container_id)
-                      when "WikiPage"
-                        WikiPage.find RedmineMerge::Mapper.get_new_wiki_page_id(source_attachment.container_id)
-                      when "Project"
-                        Project.find RedmineMerge::Mapper.get_new_project_id(source_attachment.container_id)
-                      when "Version"
-                        Version.find RedmineMerge::Mapper.get_new_version_id(source_attachment.container_id)
-                      when "News"
-                        News.find RedmineMerge::Mapper.get_new_news_id(source_attachment.container_id)
-                      end
+      puts "  Migrating attachment for #{source.container}"
+      Attachment.create!(source.attributes) do |a|
+        a.author    = SourceUser.find_target(source.author)
+        a.container = source.container.class.find_target(source.container)
       end
     end
   end
