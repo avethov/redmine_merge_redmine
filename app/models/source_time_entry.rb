157926914718c4388818f1e39dfcae978ff369c2
Arthur Andersen <leoc.git@gmail.com>
2015-06-17 10:23:49 +0200

    Fix SourceTimeEntry for Redmine 3

--- app/models/source_time_entry.rb
+++ app/models/source_time_entry.rb
@@ -2,34 +2,40 @@ class SourceTimeEntry < ActiveRecord::Base
   include SecondDatabase
   self.table_name = 'time_entries'
 
-  belongs_to :user, :class_name => 'SourceUser', :foreign_key => 'user_id'
-  belongs_to :project, :class_name => 'SourceProject', :foreign_key => 'project_id'
-  belongs_to :issue, :class_name => 'SourceIssue', :foreign_key => 'issue_id'
-  belongs_to :activity, :class_name => 'SourceEnumeration', :foreign_key => 'activity_id'
-  
+  belongs_to :user,     class_name: 'SourceUser',        foreign_key: 'user_id'
+  belongs_to :project,  class_name: 'SourceProject',     foreign_key: 'project_id'
+  belongs_to :issue,    class_name: 'SourceIssue',       foreign_key: 'issue_id'
+  belongs_to :activity, class_name: 'SourceEnumeration', foreign_key: 'activity_id'
+
+  def self.find_target(source)
+    return nil unless source
+    TimeEntry.where(
+      user:       SourceUser.find_target(source.user),
+      project:    SourceProject.find_target(source.project),
+      activity:   SourceEnumeration.find_target(source.activity),
+      issue:      SourceIssue.find_target(source.issue),
+      hours:      source.hours,
+      spent_on:   source.spent_on,
+      created_on: source.created_on
+    ).first
+  end
 
   def self.migrate
-    all.each do |source_time_entry|
-      
-      puts "TimeEntry id: #{source_time_entry.id}"
-      puts "  hours: #{source_time_entry.hours}"
-      
-      # Validation does not allow a value of 0 for hours - Hack to set it to a very low value
-      if source_time_entry.hours == 0
-        source_time_entry.hours = 0.01
+    all.each do |source|
+      target = find_target(source)
+      if target
+        puts "  Skipping existing time entry: #{source.hours}h by #{source.user}"
+        next
       end
-      
-      time_entry = TimeEntry.create!(source_time_entry.attributes) do |te|
-      
-        te.user = User.find_by_login(source_time_entry.user.login)
-        te.project = Project.find(RedmineMerge::Mapper.get_new_project_id(source_time_entry.project_id))
-        te.activity = TimeEntryActivity.find_by_name(source_time_entry.activity.name)
 
-        # optional 
-        te.issue = Issue.find_by_id(RedmineMerge::Mapper.get_new_issue_id(source_time_entry.issue.id)) if source_time_entry.issue_id
-          
+      puts "  Migrating time entry: #{source.hours}h by #{source.user}"
+      TimeEntry.create!(source.attributes) do |te|
+        te.user     = SourceUser.find_target(source.user)
+        te.project  = SourceProject.find_target(source.project)
+        te.activity = SourceEnumeration.find_target(source.activity)
+        te.issue    = SourceIssue.find_target(source.issue)
+        te.hours    = 0.01 if te.hours == 0
       end
-      puts "  merged id: #{time_entry.id}"
     end
   end
 end
