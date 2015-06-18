6ee17b76707c25c56d7dd035a87a61161641c0f0
Arthur Andersen <leoc.git@gmail.com>
2015-06-18 09:10:30 +0200

    Fix STI for SourceUser
    The users table handles groups and users via the `type` column. For the
    Source models I disabled single-table inheritance.

--- app/models/source_email_address.rb
+++ app/models/source_email_address.rb
@@ -3,4 +3,15 @@ class SourceEmailAddress < ActiveRecord::Base
   self.table_name = 'email_addresses'
 
   belongs_to :user, class_name: 'SourceUser', foreign_key: 'user_id'
+
+  def self.find_or_init_target(source)
+    find_target(source) ||
+      EmailAddress.new(source.attributes.dup.except('user_id'))
+  end
+
+  def self.find_target(source)
+    return nil unless source
+    fail 'Expected SourceEmailAddress' unless source.is_a?(SourceEmailAddress)
+    EmailAddress.find_by_address(source.address)
+  end
 end
--- app/models/source_user.rb
+++ app/models/source_user.rb
@@ -1,63 +1,63 @@
-# KS - tried to use inheritance but it did not work as a result of ActiveRecord
-#      using the "type" column and the name of the class to put together a query such as:
-#
-#   SELECT "users".* FROM "users" WHERE "users"."type" IN ('SourceUser')
-# -- Note the "SourceUser" type in the query -- the only way I know not to have ActiveRecord not use the type
-#    column would be to tell it to overwrite Base.inheritance_column but this could cause issues for Redmine.
-#
-#class SourceUser < SourcePrincipal
-
 class SourceUser < ActiveRecord::Base
   include SecondDatabase
 
-  self.table_name = "users"
+  self.table_name = 'users'
 
-  has_and_belongs_to_many :groups, :class_name => 'SourceGroup', :join_table => 'groups_users', :foreign_key => 'user_id', :association_foreign_key => 'group_id'
-  has_many :email_addresses, class_name: 'SourceEmailAddress'
+  # Override inheritance for Source models does not change anythong on
+  # the target redmine models.
+  self.inheritance_column = :_type_disabled
 
-  def self.find_target(user, options = { fail: true })
-    target = User.where(login: user.login).first ||
-             User.having_mail(user.mails).first
+  has_and_belongs_to_many :groups, class_name: 'SourceGroup', join_table: 'groups_users', foreign_key: 'user_id', association_foreign_key: 'group_id'
+  has_many :email_addresses, class_name: 'SourceEmailAddress', foreign_key: 'user_id'
+  has_one :email_address, -> { where is_default: true }, class_name: 'SourceEmailAddress', foreign_key: 'user_id', autosave: true
+
+  def mails
+    email_addresses.pluck(:address)
+  end
+
+  def mail
+    email_address.try(:address)
+  end
+
+  def to_s
+    "#{firstname} #{lastname}"
+  end
+
+  def self.find_target(source, options = { fail: true })
+    unless source.is_a?(SourceUser) || source.is_a?(SourcePrincipal)
+      fail "Expected SourceUser or SourcePrincipal got #{source.class}"
+    end
+    target = User.where(login: source.login).first ||
+             User.having_mail(source.mails).first
     if target.nil? && options[:fail]
-      fail "No target found with mails #{user.mails.inspect} or login `#{user.login}`"
+      fail "No target found with mails #{source.mails.inspect} or login `#{source.login}`"
     end
     target
   end
 
-  # ActiveRecord allows class to pull entries out of the database
   def self.migrate
     where(type: 'User').each do |source|
-      if SourceUser.find_target(source, fail: false)
-        puts "  Skipping existing user #{source}"
+      target = SourceUser.find_target(source, fail: false)
+      if target
+        puts "  Skipping existing user. #{source} (#{source.mail}) => #{target} (#{target.mail})"
         next
       end
 
-      puts "  Migrating user #{source}"
-      User.create!(source.attributes) do |target|
-        target.login = source.login
-        target.admin = source.admin
-        target.hashed_password = source.hashed_password
-
-        target.email_addresses = source.email_addresses.map do |source_mail|
-          SourceEmailAddress.find_target(source_mail) ||
-            begin
-              mail = EmailAddress.new(source_mail.attributes.dup.except('user_id'))
-              mail.created_on = DateTime.now
-              mail.updated_on = DateTime.now
-              mail
-            end
-        end
-
-        if target.email_addresses.empty?
-          target.email_address =
-            begin
-              mail = EmailAddress.new(address: "#{source.login}@redmine-merge.com", notify: false, is_default: true)
-              mail.created_on = DateTime.now
-              mail.updated_on = DateTime.now
-              mail
-            end
+      puts "  Migrating user #{source} (#{source.mail})"
+      target = User.create!(source.attributes) do |u|
+        u.login = source.login
+        u.admin = source.admin
+        u.hashed_password = source.hashed_password
+        u.email_address = SourceEmailAddress.find_or_init_target(source.email_address)
+        unless u.email_address
+          u.email_address = EmailAddress.new(
+            address: source.mail || "#{source.login}@redmine-merge.com",
+            notify: false,
+            is_default: true)
         end
       end
+
+      puts "    target user #{target} (#{target.mail})"
     end
   end
 end
