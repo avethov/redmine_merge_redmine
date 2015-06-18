3e274e870763af8aacc1fa49a8724279b9d80720
Arthur Andersen <leoc.git@gmail.com>
2015-06-18 09:38:48 +0200

    Disable `inheritance_column` for all source models
    Since we only want to grab the data from the source database and pipe it
    into the target database we can easily disable single-table inheritance
    for all source models. STI is kept enabled for all target models.

--- lib/second_database.rb
+++ lib/second_database.rb
@@ -3,6 +3,7 @@ module SecondDatabase
   def self.included(base)
     base.class_eval do
       establish_connection :source_redmine
+      self.inheritance_column = :_type_disabled
     end
   end
 end
